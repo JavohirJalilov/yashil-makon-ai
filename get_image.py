b488f87d4a7a1f1e79369c4dfd9f63b8d814e83e
Jalilov <javohirjalilov777@gmail.com>
2022-06-26 01:40:57 +0500

    create function

--- get_image.py
+++ get_image.py
@@ -1,3 +1,16 @@
 import requests
 import numpy as np
 from PIL import Image
+
+def get_image(url,api_key,lon,lat,zoom):
+    """
+    Extract a marked location from a map using the Google Maps API.
+
+    Args:
+        url(str): Google Maps API
+        api_key(str): access key
+        lon(str or int): degrees of longitude
+        lat(str or int): degrees of latitude
+    Returns:
+        numpyArray: image
+    """
